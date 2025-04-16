6b570172338ea098a85a827b5a5c42698239cfda
Asmitha24 <130919199+Asmitha24@users.noreply.github.com>
2025-04-17 00:33:00 +0530

    load_clean_data.py

--- SmartEnergy_DevOps-1/load_clean_data.py
+++ SmartEnergy_DevOps-1/load_clean_data.py
@@ -1,36 +1,27 @@
 """Module for loading and cleaning energy data."""
 import pandas as pd
 
-
-csv_file = "energy_dataset.csv"  # Adjust if necessary
-
+CSV_FILE = "energy_dataset.csv"  # Adjust if necessary
 
 print("🔹 Loading dataset...")
-df = pd.read_csv(csv_file)
-
+df = pd.read_csv(CSV_FILE)
 
 print("\n✅ Dataset Loaded Successfully! First 5 Rows:")
 print(df.head())
 
-
 print("\n🔹 Dataset Information:")
 print(df.info())
-
 
 print("\n🔹 Missing Values in Each Column:")
 print(df.isnull().sum())
 
-
 df.dropna(inplace=True)
-
 
 if 'timestamp' in df.columns:
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-
 if 'energy_consumption' in df.columns:
     df['energy_consumption'] = df['energy_consumption'].astype(float)
-
 
 df.to_csv("energy_dataset.csv", index=False)
 
