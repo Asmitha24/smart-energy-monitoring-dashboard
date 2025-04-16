327bfa0170486f37635b074366a19744940cc22e
Asmitha24 <130919199+Asmitha24@users.noreply.github.com>
2025-04-17 01:10:40 +0530

    Update test_app.py

--- SmartEnergy_DevOps-1/test_app.py
+++ SmartEnergy_DevOps-1/test_app.py
@@ -2,7 +2,7 @@
 import pytest
 from app import app  # Import your Flask app instance
 
-@pytest.fixture()
+@pytest.fixture(name="test_client")
 def get_client():
     """
     Fixture to create a test client for the Flask app.
@@ -11,21 +11,21 @@ def get_client():
     with app.test_client() as client:
         yield client
 
-def test_index_route(get_client):
+def test_index_route(index_client):
     """
     Test the main index route ('/').
     Checks for a 200 status code and expected content.
     """
-    response = get_client.get('/')
+    response = index_client.get('/')
     assert response.status_code == 200
     assert b"<!DOCTYPE html>" in response.data
 
-def test_download_csv_route(get_client):
+def test_download_csv_route(download_client):
     """
     Test the download CSV route ('/download').
     Checks for 200 status, correct content type, and filename.
     """
-    response = get_client.get('/download')
+    response = download_client.get('/download')
     assert response.status_code == 200
     assert response.content_type == 'text/csv'
     assert response.headers['Content-Disposition'] == (
